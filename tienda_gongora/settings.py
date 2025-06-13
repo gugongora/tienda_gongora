0c439261e527ebe536236d67b2940cc5c2013633
Miguel Astorga <migu.astorga@duocuc.cl>
2025-06-12 22:37:29 -0400

    Agrega dependencias nuevas

--- tienda_gongora/settings.py
+++ tienda_gongora/settings.py
@@ -133,27 +133,27 @@ WSGI_APPLICATION = 'tienda_gongora.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'tienda_gongora',
-#        'USER': 'root',
-#        'PASSWORD': 'eldiablo1',  # pon tu contraseña si es que tienes
-#        'HOST': '127.0.0.1',
-#        'PORT': '3306',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tienda_gongora',
-        'USER': 'django_user',
-        'PASSWORD': 'clave_segura_2025',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-} 
+       'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tiendagongora',
+        'USER': 'root',
+        'PASSWORD': 'eldiablo1',  # pon tu contraseña si es que tienes
+        'HOST': '127.0.0.1',
+       'PORT': '3306',
+   }
+}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'tienda_gongora',
+#        'USER': 'django_user',
+#        'PASSWORD': 'clave_segura_2025',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#    }
+#} 
 
 
 # Password validation
